a05f6230d3799e335fda60fb6f36ede85130a6f8
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-08-27 10:46:46 -0600

    Create BaseDashPlayer.py

---
+++ BaseDashPlayer.py
@@ -0,0 +1,37 @@
+import pygame 
+import math
+
+pw, ph = 10, 20
+px, py = 50, 50
+vx, vy = 5, 0
+g = 0.5
+jmp = -10
+jmp2 = -15
+jumping = False
+double_jump = False
+jump_pressed = False
+
+class player:
+#    def__init__(self):
+
+#keys = pygame.key.get_pressed()
+
+#Physics-------------------------------------------------------------------
+
+    ''' 
+#Player movement
+if keys[pygame.K_LEFT]:
+    px -= vx
+    #?
+if not jumping and keys[pygame.K_UP]:
+    vy = jmp
+    jumping = True
+
+vy += g
+py += vy
+
+if py >= 600 - ph:
+    py = 600 - ph
+    jumping = False
+    vy = 0
+    '''
